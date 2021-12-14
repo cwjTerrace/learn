d8390a1bf6da400510c009e12b6da80e64f88449
machixian <machixian@myweimai.com>
2021-12-14 22:23:57 +0800

    deep clone

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,34 @@ import flat from "./utils/flat";
 import deepClone from "./utils/deep_clone";
 
 const flatArr = flat([[1, 2, 3, , , [6666, 7777]], 2, 3]);
-console.log(flatArr);
-
-deepClone({ [Symbol("a")]: "a" });
+console.log("结果:", flatArr);
+const symbol = Symbol("symbol");
+const obj = {
+  [symbol]: "symbol",
+  undefined: undefined,
+  number: 1,
+  string: "string",
+  function: () => console.log(1),
+  obj: {
+    a: "b",
+    b: {
+      [symbol]: "symbol",
+      undefined: undefined,
+      number: 1,
+      string: "string",
+      function: () => console.log(1)
+    }
+  },
+  arr: ["1", "2", "3"]
+};
+// 循环引用
+// obj.aaa = obj;
+console.log(obj[symbol]);
+const deepCloneObj = deepClone(obj);
+console.log("结果:", deepCloneObj, obj.function == deepCloneObj.function);
+const deepCloneObj2: any = {};
+Object.assign(deepCloneObj2, obj);
+console.log("结果:", deepCloneObj2, obj.obj.b == deepCloneObj2.obj.b);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/utils/deep_clone.ts
+++ src/utils/deep_clone.ts
@@ -1,7 +1,24 @@
 /**
- * 实现一个数组拍平
+ * 实现一个神拷贝
  */
-
+const isType = (o: any, type: string) => Object.prototype.toString.call(o) === `[object ${type}]`;
 export default function deepClone(obj: any) {
-  // TODO
+  if (isType(obj, "Object")) {
+    const newObj: any = {};
+    Object.keys(obj).forEach((key) => {
+      newObj[key] = deepClone(obj[key]);
+      console.log(obj[key]);
+    });
+    return newObj;
+  }
+
+  if (isType(obj, "Array")) {
+    const newArray: any[] = [];
+    obj.forEach((e: any) => {
+      newArray.push(e);
+    });
+    return newArray;
+  }
+
+  return obj;
 }
