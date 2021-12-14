06b2375d1566bfde9e1d314c68426b30b5f1b29d
machixian <machixian@myweimai.com>
2021-12-14 17:00:51 +0800

    feat: 递归

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./layout";
 
+import flat from "./utils/flat";
+import deepClone from "./utils/deep_clone";
+
+const flatArr = flat([[1, 2, 3, [6666, 7777]], 2, 3]);
+console.log(flatArr);
+
+deepClone({ [Symbol("a")]: "a" });
+
 ReactDOM.render(
   <React.StrictMode>
-    <Layout>1231231</Layout>
+    <Layout>new flatArr : {JSON.stringify(flatArr)}</Layout>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/utils/flat.ts
+++ src/utils/flat.ts
@@ -4,4 +4,19 @@
 
 export default function flat(arr: any[], dept = 1) {
   // TODO
+  const newArr: any[] = [];
+  // 递归
+  const innerFlat = (tempArr: any[], newDept: number) => {
+    tempArr.forEach((element) => {
+      if (Array.isArray(element) && newDept > 0) {
+        innerFlat(element, --newDept);
+      } else {
+        newArr.push(element);
+      }
+    });
+  };
+
+  innerFlat(arr, dept);
+
+  return newArr;
 }
