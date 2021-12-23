b64413ca4d1970726c72321fcb0d1910a200ef80
machixian <machixian@myweimai.com>
2021-12-23 20:39:28 +0800

    feat: flat

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,41 +1,34 @@
 import getType from "./get_type";
 
-export type Value = string | number | boolean | undefined | null | Function;
-export type Mapping = Record<string, any>;
+export type Value = string | number | boolean | undefined | null;
+export type Mapping = Record<string, unknown>;
 export interface ArgumentArray extends Array<Argument> {}
 export type Argument = Value | Mapping | ArgumentArray;
 
-const classnames = (...args: ArgumentArray): any => {
+const classnames = (...args: ArgumentArray): string => {
   let r: Array<string | number> = [];
 
-  function toArr(...args: ArgumentArray): any {
-    args.forEach((e) => {
-      if (getType(e) === "Array") {
-        const arr = e as ArgumentArray;
-        arr.forEach((element) => {
+  function toArr(item: Mapping | Value) {
+    if (getType(item) === "Object") {
+      const obj: Mapping = item as Mapping;
+      Object.keys(obj).forEach((element) => {
+        if (obj[element]) {
           toArr(element);
-        });
-      }
-      if (getType(e) === "Object") {
-        const obj: Mapping = e as Mapping;
-        Object.keys(obj).forEach((element) => {
-          if (obj[element]) {
-            toArr(element);
-          }
-        });
-      }
-      if (["String", "Number"].includes(getType(e)!)) {
-        r.push(e as string | number);
-      }
-      if ("Function" === getType(e)) {
-        r.push(classnames((e as Function)()));
-      }
-    });
+        }
+      });
+    }
+    if (item && ["String", "Number"].includes(getType(item)!)) {
+      r.push(item as string | number);
+    }
   }
 
-  toArr(args);
+  const newArr = args.flat(Infinity);
 
-  return r.filter((i) => !!i).join(" ");
+  newArr.forEach((e) => {
+    toArr(e);
+  });
+
+  return r.join(" ");
 };
 
 export default classnames;
