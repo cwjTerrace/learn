2759e2ff5241a5aa0ab182cfe5e45c12b96f21f2
machixian <machixian@myweimai.com>
2022-01-03 20:17:41 +0800

    feat: 目录优化

--- vue-ui/vite.config.ts
+++ vue-ui/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/learn/",
-  plugins: [react()],
+  root: __dirname,
   alias: {
     "balm-ui-plus": "balm-ui/dist/balm-ui-plus.js",
     "balm-ui-css": "balm-ui/dist/balm-ui.css"
