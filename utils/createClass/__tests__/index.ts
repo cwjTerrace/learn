2c294d6fd4e385a81bcccb26baa7e70ce042f066
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-03-30 20:48:34 +0800

    feat: createClass

---
+++ utils/createClass/__tests__/index.ts
@@ -0,0 +1,126 @@
+/* global describe, it */
+import * as assert from "assert";
+import createClass from "..";
+
+const [prefix, classNames] = createClass("test");
+
+describe("classNames", function () {
+  it("should aaa ", function () {
+    assert.equal(prefix, "kc-test");
+  });
+
+  it("keeps object keys with truthy values", function () {
+    assert.equal(
+      classNames({
+        a: true,
+        b: false,
+        c: 0,
+        d: null,
+        e: undefined,
+        f: 1
+      }),
+      "a f"
+    );
+  });
+
+  it("joins arrays of class names and ignore falsy values", function () {
+    assert.equal(classNames("a", 0, null, undefined, true, 1, "b"), "a 1 b");
+  });
+
+  it("supports heterogenous arguments", function () {
+    assert.equal(classNames({ a: true }, "b", 0), "a b");
+  });
+
+  it("should be trimmed", function () {
+    assert.equal(classNames("", "b", {}, ""), "b");
+  });
+
+  it("returns an empty string for an empty configuration", function () {
+    assert.equal(classNames({}), "");
+  });
+
+  it("supports an array of class names", function () {
+    assert.equal(classNames(["a", "b"]), "a b");
+  });
+
+  it("joins array arguments with string arguments", function () {
+    assert.equal(classNames(["a", "b"], "c"), "a b c");
+    assert.equal(classNames("c", ["a", "b"]), "c a b");
+  });
+
+  it("handles multiple array arguments", function () {
+    assert.equal(classNames(["a", "b"], ["c", "d"]), "a b c d");
+  });
+
+  it("handles arrays that include falsy and true values", function () {
+    assert.equal(classNames(["a", 0, null, undefined, false, true, "b"]), "a b");
+  });
+
+  it("handles arrays that include arrays", function () {
+    assert.equal(classNames(["a", ["b", "c"]]), "a b c");
+  });
+
+  it("handles arrays that include objects", function () {
+    assert.equal(classNames(["a", { b: true, c: false }]), "a b");
+  });
+
+  it("handles deep array recursion", function () {
+    assert.equal(classNames(["a", ["b", ["c", { d: true }]]]), "a b c d");
+  });
+
+  it("handles arrays that are empty", function () {
+    assert.equal(classNames("a", []), "a");
+  });
+
+  it("handles nested arrays that have empty nested arrays", function () {
+    assert.equal(classNames("a", [[]]), "a");
+  });
+
+  it("handles all types of truthy and falsy property values as expected", function () {
+    assert.equal(
+      classNames({
+        // falsy:
+        null: null,
+        emptyString: "",
+        noNumber: NaN,
+        zero: 0,
+        negativeZero: -0,
+        false: false,
+        undefined: undefined,
+
+        // truthy (literally anything else):
+        nonEmptyString: "foobar",
+        whitespace: " ",
+        function: Object.prototype.toString,
+        emptyObject: {},
+        nonEmptyObject: { a: 1, b: 2 },
+        emptyList: [],
+        nonEmptyList: [1, 2, 3],
+        greaterZero: 1
+      }),
+      "nonEmptyString whitespace function emptyObject nonEmptyObject emptyList nonEmptyList greaterZero"
+    );
+  });
+
+  it("handles toString() method defined on object", function () {
+    assert.equal(
+      classNames({
+        toString: function () {
+          return "classFromMethod";
+        }
+      }),
+      "classFromMethod"
+    );
+  });
+
+  it("handles toString() method defined inherited in object", function () {
+    var Class1 = function () {};
+    var Class2: any = function () {}; // TODO 先给个any顶一下
+    Class1.prototype.toString = function () {
+      return "classFromMethod";
+    };
+    Class2.prototype = Object.create(Class1.prototype);
+
+    assert.equal(classNames(new Class2()), "classFromMethod");
+  });
+});
---
+++ utils/createClass/index.ts
@@ -0,0 +1,40 @@
+export type Mod = string | number | null | undefined | boolean | { [key: string]: any };
+export type Mods = Mod | Mod[];
+
+const gen = (name: string, mods: Mods): string[] => {
+  if (typeof mods === "boolean" || !mods) {
+    return [];
+  }
+  if (typeof mods === "string" || typeof mods === "number") {
+    return [`${name}-${mods}`];
+  }
+
+  if (Array.isArray(mods)) {
+    return mods.reduce<string[]>((ret, item) => ret.concat(gen(name, item)), []);
+  }
+  return Object.keys(mods).reduce<string[]>((ret, key) => (mods[key] ? ret.concat(gen(name, key)) : ret), []);
+};
+
+const classNames = (prefixedName: string) => {
+  return (...args: Mods[]): string => {
+    const r: string[] = [];
+    args.forEach((el: Mods) => {
+      r.push.apply(r, gen(prefixedName, el));
+    });
+
+    return r.join(" ");
+  };
+};
+
+/**
+ * classNames helper
+ * c('text') // 'button-text'
+ * c({ disabled }) // 'button-disabled'
+ * c(['disabled', 'primary']) // 'button-disabled button-primary'
+ */
+const createClass = (name: string) => {
+  const prefixedName = `kc-${name}`;
+  return [prefixedName, classNames(prefixedName)] as const;
+};
+
+export default createClass;
