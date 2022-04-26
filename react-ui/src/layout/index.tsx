20689ef85e584655909458ecc06dc03979d76118
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-04-26 14:24:41 +0800

    refact: update type react 18

--- react-ui/src/layout/index.tsx
+++ react-ui/src/layout/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 
-const Layout: React.FC = (props) => {
+const Layout: React.FC<PropsWithChildren<{}>> = (props) => {
   return <div className="layout">{props.children}</div>;
 };
 
