299af79b14cdaa63cf0e85e1393c540b221489a6
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-05-06 14:56:54 +0800

    feat: add blog

--- vue-ui/src/layout/index.vue
+++ vue-ui/src/layout/index.vue
@@ -5,6 +5,15 @@ export default defineComponent({
   name: "layout",
   setup(props) {
     console.log(props);
+    function aaa(params: any) {
+      console.log("aaaaaa", params);
+    }
+    aaa`
+      background-color: hotpink;
+      &:hover {
+        color: red;
+      }
+    `;
     return () => <div className="layout">1</div>;
   }
 });
