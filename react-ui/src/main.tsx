00da306f99629bc0e912348713c934be41db0ceb
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-06-01 16:30:43 +0800

    代码移到test中

--- react-ui/src/main.tsx
+++ react-ui/src/main.tsx
@@ -4,43 +4,13 @@ import Layout from "./layout";
 
 import SvgDemo from "./pages/svg-demo";
 
-import { deepClone, classnames, myPromise } from "@/utils";
+import { myPromise } from "@/utils";
 
 import { promiseRecursive } from "@/topic";
 
 promiseRecursive().then((res) => {
   console.log("promiseRecursive", res);
 });
-
-const symbol = Symbol("symbol");
-const obj = {
-  [symbol]: "symbol",
-  symbol: Symbol("symbol"),
-  undefined: undefined,
-  number: 1,
-  string: "string",
-  function: () => console.log(1),
-  map: new Map([["key", "value"]]),
-  set: new Set("1"),
-  obj: {
-    a: "b",
-    b: {
-      [symbol]: "symbol",
-      undefined: undefined,
-      number: 1,
-      string: "string",
-      function: () => console.log(1)
-    }
-  },
-  arr: ["1", "2", "3"]
-};
-// 循环引用
-// obj.aaa = obj;
-const deepCloneObj = deepClone(obj);
-console.log("结果:", deepCloneObj, obj.symbol == deepCloneObj.symbol);
-const deepCloneObj2: any = {};
-Object.assign(deepCloneObj2, obj);
-// console.log("结果:", deepCloneObj2, obj.obj.b == deepCloneObj2.obj.b);
 
 new myPromise((resolve, rejects) => {
   console.log(Date.now());
@@ -55,7 +25,6 @@ new myPromise((resolve, rejects) => {
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <Layout>
-    <div>{classnames(obj)}</div>
     <SvgDemo color="#333" />
   </Layout>
 );
