f1954de4d2d2ec2679ff5ed47d3f4da2247177e0
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-06-20 18:29:57 +0800

    feat: update vite

--- vue-ui/shims-tsx.d.ts
+++ vue-ui/shims-tsx.d.ts
@@ -1,5 +1,8 @@
-import Vue from "vue";
+/// <reference types="vite/client" />
 
 declare module "*.vue" {
-  export default Vue;
+  import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
