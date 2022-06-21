a5c175582edf3d5c5d05e03f88dcb915d6f62e4d
ranrui.cwj <ranrui.cwj@alibaba-inc.com>
2022-06-21 10:35:20 +0800

    remove babel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//   base: "/learn/",
+  //   base: "/learn/",
   build: {
     target: "es2021"
   },
