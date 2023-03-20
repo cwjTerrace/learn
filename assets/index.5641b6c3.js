203841912e99f5578f2a1dc646387fca99a9eb12
cwjTerrace <cwjTerrace@163.com>
2023-03-20 06:15:48 +0000

    Deploying to gh-pages from @ cwjTerrace/learn@4db9d497098ce09f019a5833488716145cd603b9 🚀

--- assets/index.5641b6c3.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as o}from"./index.86b19792.js";const n=e=>new Promise(s=>setTimeout(()=>s({id:e,name:`\u7528\u6237${e}`,age:10+e}),3e3)),c=e=>{const[s,r]=t.exports.useState({});return t.exports.useEffect(()=>{n(e.id).then(u=>r(u))},[e.id]),console.log(s),o("div",{children:["\u5F53\u524D\u7528\u6237\u662F: ",s.name]})};export{c as default};
